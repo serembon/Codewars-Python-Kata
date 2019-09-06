eae1d1ed73fcd6ab7d4261c94c2d0c6f990230fa
serembon <22729616+serembon@users.noreply.github.com>
2019-09-06 16:10:31 +0300

    new solution

---
+++ Rot13.py
@@ -0,0 +1,13 @@
+"""ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the
+alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special
+characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet
+should be shifted, like in the original Rot13 "implementation".
+
+Please note that using ```encode``` is considered cheating."""
+
+
+def rot13(message):  # My and best way solution
+
+    return ''.join([chr(ord(n) + (13 if 'Z' < n < 'n' or n < 'N' else -13)) if n.isalpha() else n for n in message])
